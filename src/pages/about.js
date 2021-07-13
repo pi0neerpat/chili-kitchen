0e07f7cd6ad8fc207deb043fcb6ca559c904e046
patrick <blockchainbuddha@gmail.com>
2021-07-13 18:48:11 -0400

    test commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Layout } from '../components/common'
+import React from "react";
+import { Layout } from "../components/common";
 
 const About = () => (
     <Layout>
         <div className="container">
             <article className="content" style={{ textAlign: `center` }}>
                 <h1 className="content-title">About</h1>
-                <section className="content-body">
-                    About
-                </section>
+                <section className="content-body">About TEST!</section>
             </article>
         </div>
     </Layout>
-)
+);
 
-export default About
+export default About;
