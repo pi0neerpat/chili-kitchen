92d75d87bb00817180058c3a1cb4f75dceb60298
chair <vrycmfy@protonmail.com>
2021-07-15 00:28:47 -0400

    Minimum viable scoville meter

---
+++ src/components/ScovilleMeter.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Img from "gatsby-image";
+import GreyScoville from "../images/grey-scoville.png"
+import RedScoville from "../images/red-scoville.png"
+
+const ScovilleMeter = () => (
+	<div className="scoville-container">
+		<img className="red-scoville-chili" src={RedScoville} alt="Red Scoville Chili" />
+		<img className="red-scoville-chili" src={RedScoville} alt="Red Scoville Chili" />
+		<img className="red-scoville-chili" src={RedScoville} alt="Red Scoville Chili" />
+		<img className="red-scoville-chili" src={RedScoville} alt="Red Scoville Chili" />
+		<img className="red-scoville-chili" src={RedScoville} alt="Red Scoville Chili" />
+	</div>
+)
+
+export default ScovilleMeter;
